7d21ab988fc1224d7b97c91492066e774771832f
“johanneshi” <johannes.hirschbeck@hm.edu>
2023-03-19 21:12:30 +0100

    added output and changed input to floats

--- exercises/01_Datentypen/01_Datatypes_excercise.py
+++ exercises/01_Datentypen/01_Datatypes_excercise.py
@@ -8,29 +8,29 @@ geometric_body = input(
 
 if geometric_body == 'cube':
     # input of sidelength
-    side_a = int(
-        input("Please state the length of the sides of the cube in integers: "))
+    side_a = float(
+        input("Please state the length of the sides of the cube as float: "))
     # calculate volume and surface
     volume = side_a**3
     surface = side_a**2 * 6
 
 elif geometric_body == 'cuboid':
     # input of sidelengths
-    side_a = int(
-        input("Please state the length of the side 'a' of the cuboid in integers: "))
-    side_b = int(
-        input("Please state the length of the side 'b' of the cuboid in integers: "))
-    side_c = int(
-        input("Please state the length of the side 'c' of the cuboid in integers: "))
+    side_a = float(
+        input("Please state the length of the side 'a' of the cuboid as float: "))
+    side_b = float(
+        input("Please state the length of the side 'b' of the cuboid as float: "))
+    side_c = float(
+        input("Please state the length of the side 'c' of the cuboid as float: "))
     # calculate volume and surface
     volume = side_a*side_b*side_c
     surface = 2 * side_a * side_b + 2 * side_a * side_b + 2 * side_b * side_c
 
 elif geometric_body == 'square pyramid':
-    side_a = int(
-    input("Please state the length of the sides of the footprint of the quadratic pyramid in integers: "))
-    height_h = int(
-    input("Please state the height of the sides of the quadratic pyramid in integers: "))
+    side_a = float(
+    input("Please state the length of the sides of the footprint of the quadratic pyramid as float: "))
+    height_h = float(
+    input("Please state the height of the quadratic pyramid as float: "))
     ##calculate height of one sidetriangle to calculate surface
     height_ha = math.sqrt(height_h**2 + (side_a/2)**2)
 
@@ -47,3 +47,6 @@ else:
         "Please name the geometrical object of which you want to calculate the volume and surface: 'cube' , 'cuboid' or 'square pyramid': ")
 
 
+#print the volume and the surface of the geometric body to console
+print("The volume of a", geometric_body, " is: ", str(volume))
+print("The surface of a", geometric_body, " is: ", str(surface))
