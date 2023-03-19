cbc99f9275874f9ce0efc31ebf59d1ccedbc7031
“johanneshi” <johannes.hirschbeck@hm.edu>
2023-03-19 20:50:22 +0100

    Selection of the geometric body implemented

---
+++ exercises/01_Datentypen/01_Datatypes_excercise.py
@@ -0,0 +1,15 @@
+import math
+
+#select geometrical body
+geometric_body = input("Please name the geometrical object of which you want to calculate the volume and surface: 'cube' , 'cuboid' or 'square pyramid': ")
+
+#differentiation of the input and calculation of volume and surface
+if geometric_body == 'cube':
+    pass
+elif geometric_body == 'cuboid':
+    pass
+elif geometric_body == 'square pyramid':
+    pass
+else:
+    print("Not supported input. Please do only select one of the three options.")
+    geometric_body = input("Please name the geometrical object of which you want to calculate the volume and surface: 'cube' , 'cuboid' or 'square pyramid': ")
