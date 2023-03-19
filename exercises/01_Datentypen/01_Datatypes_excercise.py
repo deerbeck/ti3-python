b2b5d3dd5ea6d9fe5867344ccebfa380a337c074
“johanneshi” <johannes.hirschbeck@hm.edu>
2023-03-19 21:05:58 +0100

    calculation of volume and surface for different bodys

--- exercises/01_Datentypen/01_Datatypes_excercise.py
+++ exercises/01_Datentypen/01_Datatypes_excercise.py
@@ -1,15 +1,49 @@
 import math
 
-#select geometrical body
-geometric_body = input("Please name the geometrical object of which you want to calculate the volume and surface: 'cube' , 'cuboid' or 'square pyramid': ")
+# select geometrical body
+geometric_body = input(
+    "Please name the geometrical object of which you want to calculate the volume and surface: 'cube' , 'cuboid' or 'square pyramid': ")
 
-#differentiation of the input and calculation of volume and surface
+# differentiation of the geometric bodys
+
 if geometric_body == 'cube':
-    pass
+    # input of sidelength
+    side_a = int(
+        input("Please state the length of the sides of the cube in integers: "))
+    # calculate volume and surface
+    volume = side_a**3
+    surface = side_a**2 * 6
+
 elif geometric_body == 'cuboid':
-    pass
+    # input of sidelengths
+    side_a = int(
+        input("Please state the length of the side 'a' of the cuboid in integers: "))
+    side_b = int(
+        input("Please state the length of the side 'b' of the cuboid in integers: "))
+    side_c = int(
+        input("Please state the length of the side 'c' of the cuboid in integers: "))
+    # calculate volume and surface
+    volume = side_a*side_b*side_c
+    surface = 2 * side_a * side_b + 2 * side_a * side_b + 2 * side_b * side_c
+
 elif geometric_body == 'square pyramid':
-    pass
+    side_a = int(
+    input("Please state the length of the sides of the footprint of the quadratic pyramid in integers: "))
+    height_h = int(
+    input("Please state the height of the sides of the quadratic pyramid in integers: "))
+    ##calculate height of one sidetriangle to calculate surface
+    height_ha = math.sqrt(height_h**2 + (side_a/2)**2)
+
+    volume = (side_a**2 * height_h)/3
+    surface = (side_a**2 + side_a*height_ha*2)
+    
+
+
+
+# new input if previous input was faulty
 else:
     print("Not supported input. Please do only select one of the three options.")
-    geometric_body = input("Please name the geometrical object of which you want to calculate the volume and surface: 'cube' , 'cuboid' or 'square pyramid': ")
+    geometric_body = input(
+        "Please name the geometrical object of which you want to calculate the volume and surface: 'cube' , 'cuboid' or 'square pyramid': ")
+
+
