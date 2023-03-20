52c6eb6eee9366e8f209f9711e3a43886955cb65
“johanneshi” <johannes.hirschbeck@hm.edu>
2023-03-20 10:06:27 +0100

    started excercise 1 of contructors and methods

---
+++ exercises/01_KonstruktorenMethoden/01_ComstructorsMethods_excercise01.py
@@ -0,0 +1,22 @@
+# starttuple
+t = (1, 1, 2, 3, 5, 8, 13, 21)
+print(t)
+
+#convert t to string
+string = str(t)
+print(string)
+
+#string method testing
+
+s = "das Ist ME1N teext"
+
+print(s)
+print(s.capitalize())
+print(s.count())
+print(s.find())
+print(s.isalpha())
+print(s.join())
+print(s.lower())
+print(s.replace())
+print(s.rsplit())
+print(s.split())
