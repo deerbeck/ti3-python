1f3551b7ee73aadbda6b287064568509fd08155b
“johanneshi” <johannes.hirschbeck@hm.edu>
2023-03-23 00:29:40 +0100

    started ass01_1, read data and stored in list + extracted temp_code + extrated codeword

---
+++ assignments/assignment01/ass01_1.py
@@ -0,0 +1,13 @@
+import os
+
+
+#open text file, read all lines and store each line as element of a list in text
+text = open(".\\assignments\\assignment01\\text_assignment01.txt", 'r').readlines()
+
+#extract needed codewordsnipplets out of text, join them and safe it in 'temp_code'
+temp_code = text[5][2] + text[2][26:29] + text[1][:3] + text[-1][:3]
+
+#modify 'temp_code' to fit requirements
+codeword = (temp_code[::-1] * 5)[:-2][8::8]
+print(codeword)
+
