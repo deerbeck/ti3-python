43a5ccb122ef1c90e1008dd7b40655b065d4188e
“johanneshi” <johannes.hirschbeck@hm.edu>
2023-03-23 18:18:19 +0100

    final path changes

--- assignments/assignment01/ass01_1.py
+++ assignments/assignment01/ass01_1.py
@@ -1,8 +1,7 @@
 import os
 
-
 # open text file, read all lines and store each line as element of a list in text
-text = open(".\\assignments\\assignment01\\text_assignment01.txt",'r').readlines()
+text = open("text_assignment01.txt",'r').readlines()
 
 # extract needed codewordsnipplets out of text, join them and safe it in 'temp_code'
 temp_code = text[5][2] + text[2][26:29] + text[1][:3] + text[-1][:3]
