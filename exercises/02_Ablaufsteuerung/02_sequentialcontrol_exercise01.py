4ab8d6a71bd10023dc3589465320e1e1bab2169e
“johanneshi” <johannes.hirschbeck@hm.edu>
2023-03-27 14:03:45 +0200

    started and completed exercise 1 of sequential control

---
+++ exercises/02_Ablaufsteuerung/02_sequentialcontrol_exercise01.py
@@ -0,0 +1,18 @@
+# largest common devisor
+
+# define function to calculate lcd
+
+def ggt(a: int, b: int):
+    a_start = a
+    b_start = b
+    while(a > 0 and b > 0):
+        if a > b:
+            a = a-b
+        else:
+            b = b-a
+    # print out what the lcd of a and b is using string formats and a 1 line if condition
+    print(f"ggt({a_start},{b_start}) = {a if b==0 else b}")
+
+# use while loop to run the algorithm according to the given nassi-shneiderman diagramm
+
+ggt(18, 6)
