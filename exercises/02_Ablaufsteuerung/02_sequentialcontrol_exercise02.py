c7eaab8444add15d3ac1c2f0b5694a8f5ba8ac04
“johanneshi” <johannes.hirschbeck@hm.edu>
2023-03-28 12:14:29 +0200

    completed taks 2 of sequentialcontrol

--- exercises/02_Ablaufsteuerung/02_sequentialcontrol_exercise02.py
+++ exercises/02_Ablaufsteuerung/02_sequentialcontrol_exercise02.py
@@ -4,8 +4,11 @@ import random
 random_values = [0]*100
 
 #get a list of 1000 random values between 0 and 99
+random_raw = [(random.randint(0,99)) for e in range(1000)]
 
-rand_raw = [(random.randint(0,99)) for e in range(1000)]
-print(rand_raw)
+#loop through possible number 0-99 an return frequency of that number in 1000
+for i in range(100):
+    random_values[i] = random_raw.count(i)
+    #print out all random values generated less than 5 times using string format.
+    if (random_values[i] < 5): print(f"Wert {i} wurde {random_values[i]} ermittelt.")
 
-#loop through each index of random_value and place a random value between 0 and 99
