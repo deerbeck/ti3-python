c1a6585571283726203fa07d6a0607efa31c4860
“johanneshi” <johannes.hirschbeck@hm.edu>
2023-03-28 12:47:31 +0200

    started task 1 of algorithm exercises

---
+++ exercises/02_Algorithmen/02_algorithms_exercise01.py
@@ -0,0 +1,6 @@
+#define dicts using given graph
+a = {"elemnt":"A", "printed": False}
+b = {"elemnt":"B", "printed": False}
+c = {"elemnt":"C", "printed": False}
+d = {"elemnt":"D", "printed": False}
+print(a)
