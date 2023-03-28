63f8b6e4f850437ab0c2eaaff8e5a188871c0f88
“johanneshi” <johannes.hirschbeck@hm.edu>
2023-03-28 20:01:07 +0200

    completed task01 of exercise expressionsassignments

---
+++ exercises/03_AusdrückeZuweisungen/03_expressionsassignments_exercise02.py
@@ -0,0 +1,15 @@
+#provided input for exercise 01
+digits = '0123456789'
+
+##print out given slicing operations:
+#Output: "345"
+print(digits[3:6])
+
+#Output "35"
+print(digits[3:6:2])
+
+#Output "0123456789"
+print(digits[::])
+
+#Output ""
+print(digits[2:4:-1])
