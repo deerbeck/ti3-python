54db47955e26542d0c4bd8432a0758af8b13ff3f
“johanneshi” <johannes.hirschbeck@hm.edu>
2023-03-28 19:46:21 +0200

    completed task 01 and 02 of algorithms

--- exercises/02_Algorithmen/02_algorithms_exercise01.py
+++ exercises/02_Algorithmen/02_algorithms_exercise01.py
@@ -1,6 +1,29 @@
-#define dicts using given graph
-a = {"elemnt":"A", "printed": False}
-b = {"elemnt":"B", "printed": False}
-c = {"elemnt":"C", "printed": False}
-d = {"elemnt":"D", "printed": False}
-print(a)
+# define dicts using given graph
+a = {"element": "A", "printed": False}
+b = {"element": "B", "printed": False}
+c = {"element": "C", "printed": False}
+d = {"element": "D", "printed": False}
+
+# add transitions to dicts via the "next" key
+a["next"] = [b]
+b["next"] = [c, d]
+c["next"] = [b]
+d["next"] = [a]
+
+
+def printnodes(nodes, level=0):
+    #print out node and 
+    print(" "*level + "{}->".format(nodes["element"]))
+    
+    #check if node was already covered and exit function if so
+    if nodes["printed"] == True:
+        return
+    #mark node as covered
+    nodes["printed"] = True
+    
+    #iterated through any following nodes
+    for e in nodes["next"]:
+        printnodes(e, level+1)
+
+
+printnodes(b)
