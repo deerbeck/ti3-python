6c60f3f462c73a2fce73c9cc1d64e6ce5b9c7383
“johanneshi” <johannes.hirschbeck@hm.edu>
2023-04-14 09:43:30 +0200

    completed task1 of assigment02 and implemented create_area() function

--- assignments/assignment02/ass02.py
+++ assignments/assignment02/ass02.py
@@ -1,3 +1,12 @@
+import numpy as np
+
+def create_area(size):
+    #check if size is in given max and min perimiters
+    if max(size) > 10 or min(size) < 2:
+        return None
+    #create empty area using list comprehension
+    else:
+        return [[' '] * size[1] for i in range(size[0])]
 
 # main in application
 if __name__=='__main__':
