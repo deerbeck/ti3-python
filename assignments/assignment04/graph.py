def2e244b6476d34f173eb3b31d55b43f24712b9
“johanneshi” <johannes.hirschbeck@hm.edu>
2023-05-23 11:16:44 +0200

    optimizing __str__() of nodes class and completed step 2 of task 3 of asgn4 and implemented find_edge() & find_node()

--- assignments/assignment04/graph.py
+++ assignments/assignment04/graph.py
@@ -1,2 +1,69 @@
+import node
+import edge
 
-# TODO
+class Graph:
+
+    def __init__(self) -> None:
+        #initialize empty nodes and edges lists
+        self.__nodes = []
+        self.__edges = []
+    
+    def new_node(self, name = None) -> node.Node:
+        #add new node object with given name to node list
+        self.__nodes.append(n := node.Node(name))
+        return n
+    
+    def new_edge(self, name = None, weight = 0) -> edge.Edge:
+        #add new edge object with given name to edge list
+        self.__edges.append(e := edge.Edge(name, weight))
+        return e
+    
+    def find_node(self, name) -> node.Node:
+        ##loop through all nodes and return node when found else None
+        for n in self.__nodes:
+            if n.name == name:
+                return n
+        
+        return None
+
+    def find_edge(self, name) -> edge.Edge:
+        ##loop through all edges and return edge when found else None
+        for e in self.__edges:
+            if e.name == name:
+                return e
+        
+        return None
+
+
+    def __str__(self) -> str:
+        ##prefill printlist with nodes decsription
+        printlist = ["Knoten:\n-------\n"]
+
+        #loop through nodes and its connections
+        for nodes in (self.__nodes):
+            printlist.append(f"{nodes}")
+        
+        #fill printlist with edges
+        printlist.append("\n\nKanten:\n-------\n")
+
+        #loop through edges and its connections
+        for edges in self.__edges:
+            printlist.append(f"{edges}")
+        
+        #return joint printlist
+        return "".join(printlist)
+                             
+                          
+        
+
+
+
+if __name__ == "__main__":
+    g = Graph()
+    n1 = g.new_node('A')
+    n2 = g.new_node()
+    e1 = g.new_edge('E', 5)
+    n1.connect(e1)
+    e1.connect(n2)
+    print(g)
+    pass
--- assignments/assignment04/node.py
+++ assignments/assignment04/node.py
@@ -16,7 +16,7 @@ class Node:
         else:
             self.__name = name
 
-        self.__next = [self]
+        self.__next = []
 
     # getter method for name
     @property
@@ -34,30 +34,46 @@ class Node:
 
     def __str__(self) -> str:
         # print out working Tree of node
-        if (n := len(self.__next)) == 1:
+        if (n := len(self.__next)) == 0:
             # print out end of working tree when no follow up nodes exist
             return f"{self.__name} <end>"
+        
+        if n == 1:
+            #in case of only 1 connection print that connection
+            return self.__name +  f" --{self.__next[0]}--> {self.__next[0].get_connect().name}\n"
+        
         else:
-            printlist = []
-            # loop through __next nodes and print out working tree of nodes
-            for i in range(n-2):
-                printlist.append(f"{self.__next[i].__name} --{self.__next[i+1]}--> {self.__next[i+1].get_connect().name}\n")
-            # return last element of working tree formated like below
-            printlist.append(
-                len(self.__next[-3].__name) * " " + " " + f"--{self.__next[-1]}--> {self.__next[-1].get_connect().name}")
-            return "".join(printlist)
+            # get max name listed for easier format use
+            maxformat = max([len(x.get_connect().name) for x in self.__next])
+            # fill printlist with first node and its connection
+            printlist = [self.__name +  f" --{self.__next[0]}--> {self.__next[0].get_connect().name}\n"]
+            # loop through edges and its connected nodes and add them to the printlist
+            for i in range(1, n-1):
+                printlist.append(f"{self.__next[i-1].get_connect().name} --{self.__next[i]}--> {self.__next[i].get_connect().name}\n")
+            #add last edge and node to printlist
+            printlist.append(maxformat * " " + " " + f"--{self.__next[-1]}--> {self.__next[-1].get_connect().name}")
+        
+        #join all elements of printlist and return them
+        return "".join(printlist)
 
 
 if __name__ == "__main__":
-    # n1 = Node('A')
-    # n2 = Node('B')
-    # n3 = Node('C')
-    # e1 = edge.Edge('E', 5)
-    # e2 = edge.Edge('F', 2)
-    # n1.connect(e1)
-    # n1.connect(e2)
-    # e1.connect(n2)
-    # e2.connect(n3)
-    # print(n1)
-    # print(n2)
+    n1 = Node('A')
+    n2 = Node()
+    n3 = Node('C')
+    n4 = Node('D')
+    e1 = edge.Edge('E', 5)
+    e2 = edge.Edge('F', 2)
+    e3 = edge.Edge('G', 7)
+    
+    n1.connect(e1)
+    n1.connect(e2)
+    n1.connect(e3)
+
+    e1.connect(n2)
+    e2.connect(n3)
+    e3.connect(n4)
+
+    print(n1)
+    print(n2)
     pass
